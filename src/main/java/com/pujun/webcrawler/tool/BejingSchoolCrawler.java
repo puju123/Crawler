6db372defee6d2d20aabb8d4956cf40483069a9d
puju123 <puju123@sina.com>
2017-04-15 15:57:56 +0800

    jiexi

--- src/main/java/com/pujun/webcrawler/tool/BejingSchoolCrawler.java
+++ src/main/java/com/pujun/webcrawler/tool/BejingSchoolCrawler.java
@@ -10,13 +10,15 @@ import org.jsoup.select.Elements;
 
 
 public class BejingSchoolCrawler extends Crawler {
-
 	@Override
 	public JSONObject parseContent(String html) {
 		JSONObject result=new JSONObject();
 		Document document=Jsoup.parse(html);
 		//总价解析
 		Elements total=document.select("div.content > div.price >span.total");
+		if (total.hasText()) {
+			result.put("price", total);
+		}
 		//基本信息解析
 		Elements baseInfo=document.select("div.base > div.content > ul");
 		if (baseInfo.hasText()) {
@@ -33,9 +35,69 @@ public class BejingSchoolCrawler extends Crawler {
 	private void extractInfo(Elements baseInfo, JSONObject result) {
 		Elements baseNode=baseInfo.first().children();
 		for (Element element : baseNode) {
-			String key=element.select("span").text();
+			String key=element.child(0).text();
 			String value=element.child(1).text();
-			result.put(key, value);
+			switch (key) {
+			case "房屋户型":
+				result.put("huxing", value);
+				break;
+			case "所在楼层":
+				result.put("louceng", value);
+				break;
+			case "建筑面积":
+				result.put("jianmian", value);
+				break;
+			case "套内面积":
+				result.put("taomian", value);
+				break;
+			case "建筑类型":
+				result.put("louxing", value);
+				break;
+			case "房屋朝向":
+				result.put("chaoxiang", value);
+				break;
+			case "建筑结构":
+				result.put("jiegou", value);
+				break;
+			case "装修情况":
+				result.put("zhuangxiu", value);
+				break;
+			case "梯户比例":
+				result.put("tihubili", value);
+				break;
+			case "供暖方式":
+				result.put("gongnuan", value);
+				break;
+			case "配备电梯":
+				result.put("hasDianti", value);
+				break;
+			case "产权年限":
+				result.put("chanquan", value);
+				break;
+			case "交易权属":
+				result.put("jiaoyiquanshu", value);
+				break;
+			case "挂牌时间":
+				result.put("guapaishijian", value);
+				break;
+			case "上次交易":
+				result.put("shangcijiaoyi", value);
+				break;
+			case "房屋用途":
+				result.put("yongtu", value);
+				break;
+			case "房屋年限":
+				result.put("chiyounianxian", value);
+				break;
+			case "产权所属":
+				result.put("suoyouquan", value);
+				break;
+			case "抵押信息":
+				result.put("diya", value);
+				break;
+			default:
+				break;
+			}
 		}
 		
 	}
